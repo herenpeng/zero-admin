e3d50dcbdb6a97e9103435b0ffe311409951e40c
herenpeng <he.ren.peng@qq.com>
2020-11-01 00:00:17 +0800

    一个通用的后台管理系统--完善系统资源扫描功能和系统日志功能

---
+++ zero-common/src/main/java/com/zero/common/service/BaseService.java
@@ -0,0 +1,12 @@
+package com.zero.common.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * Service接口的通用接口
+ *
+ * @author herenpeng
+ * @since 2020-10-30 22:05
+ */
+public interface BaseService<T> extends IService<T> {
+}
