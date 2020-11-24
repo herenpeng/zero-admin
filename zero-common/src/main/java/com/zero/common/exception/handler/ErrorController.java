4cd3271cce865af9a4c1db03fff4790796bd57fe
herenpeng <2519832582@qq.com>
2020-11-24 22:13:13 +0800

    一个通用的后台管理系统--优化系统逻辑

--- zero-common/src/main/java/com/zero/common/exception/handler/ErrorController.java
+++ zero-common/src/main/java/com/zero/common/exception/handler/ErrorController.java
@@ -1,6 +1,7 @@
 package com.zero.common.exception.handler;
 
 import com.zero.common.exception.MyExceptionEnum;
+import io.swagger.annotations.Api;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
@@ -16,9 +17,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Filter过滤器异常处理控制器
+ *
  * @author herenpeng
  * @since 2020-11-21 22:14
  */
+@Api(value = "过滤器异常处理接口", tags = "ErrorController")
 @RestController
 public class ErrorController extends BasicErrorController {
 
@@ -29,13 +33,13 @@ public class ErrorController extends BasicErrorController {
     @Override
     @RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
-        Map<String, Object> errorAttributes  = getErrorAttributes(request, ErrorAttributeOptions.of(
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.of(
                 ErrorAttributeOptions.Include.EXCEPTION,
                 ErrorAttributeOptions.Include.MESSAGE,
                 ErrorAttributeOptions.Include.STACK_TRACE,
                 ErrorAttributeOptions.Include.BINDING_ERRORS));
         HttpStatus status = getStatus(request);
-        Map<String,Object> responseData = new HashMap<>(8);
+        Map<String, Object> responseData = new HashMap<>(8);
         responseData.put("code", MyExceptionEnum.ILLEGAL_TOKEN.getCode());
         responseData.put("message", MyExceptionEnum.ILLEGAL_TOKEN.getMessage());
         return ResponseEntity.ok(responseData);
