b7cc32441289eac3435b2b94eb1fc7d029701779
herenpeng <2519832582@qq.com>
2020-11-24 22:09:54 +0800

    一个通用的后台管理系统--优化系统逻辑

--- zero-common/src/main/java/com/zero/common/swagger/config/SwaggerConfig.java
+++ zero-common/src/main/java/com/zero/common/swagger/config/SwaggerConfig.java
@@ -12,6 +12,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
+ * Swagger2配置Java类
+ *
  * @author herenpeng
  * @since 2020-09-27 20:12
  */
@@ -33,12 +35,12 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 // 标题信息
-                .title("Zero-Admin通用后台管理系统接口文档")
+                .title("zero-admin通用后台管理系统接口文档")
                 // 描述信息
                 .description("该篇接口文档是关于zero-admin通用后台管理系统的接口说明文档，对于该系统中的所有接口进行了详细的描述！")
                 // 系统版本
                 .version("1.0.0")
-                .contact(new Contact("何任鹏", "https://herenpeng.github.io", "he.ren.peng@qq.com"))
+                .contact(new Contact("herenpeng", "https://herenpeng.github.io", "he.ren.peng@qq.com"))
                 .license("前往系统")
                 .licenseUrl("https://www.baidu.com/")
                 .build();
