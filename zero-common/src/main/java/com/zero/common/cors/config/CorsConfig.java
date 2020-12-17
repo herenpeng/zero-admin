4f0c858ec6055a4689b0f622c51776a2bf6d20db
herenpeng <2519832582@qq.com>
2020-12-17 22:07:18 +0800

    一个通用的后台管理系统--优化系统

--- zero-common/src/main/java/com/zero/common/cors/config/CorsConfig.java
+++ zero-common/src/main/java/com/zero/common/cors/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://127.0.0.1:9001");
+        config.addAllowedOrigin("http://127.0.0.1:20000");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
